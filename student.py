36e0b40286497d1f6e9c8faf5fbbc95f0d2ab0eb
FHAZ-141 <fh1952298505@foxmail.com>
2026-09-14 18:25:45 +0800

    更新 student.py
    修改score代码为name

--- student.py
+++ student.py
@@ -1,6 +1,6 @@
 def show_student():
-    score = float(input("请输入学生成绩："))
-    print(f"学生成绩：{score}")
+    name = input("请输入学生姓名：")
+    print(f"学生姓名：{name}")
 
 if __name__ == "__main__":
     show_student()
